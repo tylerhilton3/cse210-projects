ca272166ba849738d4ba36ee8e9fb1e4be47e45f
tylerhilton3 <tyler.hilton@sbcglobal.net>
2024-01-13 22:33:50 -0700

    finished the assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        string letter = "";
+        if (grade >= 90) {
+            letter = "A";
+        } else if (grade >= 80) {
+            letter = "B";
+        } else if (grade >= 70) {
+            letter = "C";
+        } else if (grade >= 60) {
+            letter = "D";
+        } else {
+            letter = "F";
+        }
+
+        Console.Write($"Your grade is {letter}");
+
+        if (grade >= 70) {
+            Console.Write("Congrats on passing!");
+        } else {
+            Console.Write("Push harder next time!");
+        }
     }
 }

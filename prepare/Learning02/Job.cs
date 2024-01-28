43df2c7858bcd8338a0ba213921eddd1d52f5a6a
tylerhilton3 <tyler.hilton@sbcglobal.net>
2024-01-27 19:11:24 -0700

    finished week 3 learning

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,10 @@
+public class Job{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display() {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+    static void Main(string[] args) {
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job1.Display();
+        job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Tyler Hilton";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }

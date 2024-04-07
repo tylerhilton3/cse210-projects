18ec699b56c02c7dbd8c39939f0e02068528070c
tylerhilton3 <tyler.hilton@sbcglobal.net>
2024-04-07 16:38:33 -0600

    finished week 7 learning

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _name;
+    private string _topic;
+
+    public Assignment(string name, string topic) {
+        _name = name;
+        _topic = topic;
+    }
+
+    public string GetStudentName() {
+        return _name;
+    }
+
+    public string GetTopic() {
+        return _topic;
+    }
+
+    public string GetSummary() {
+        return _name + " - " + _topic;
+    }
+}
